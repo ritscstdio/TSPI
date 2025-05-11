d4a8e888eb494292dc57732e319b4c04a72e7a46
ritscstdio <ritscstdio@gmail.com>
2025-05-12 03:36:58 +0800

    Backup

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -24,42 +24,42 @@ $current_page = basename($_SERVER['PHP_SELF']);
     <nav class="sidebar-nav">
         <ul>
             <li class="<?php echo $current_page === 'index.php' ? 'active' : ''; ?>">
-                <a href="index.php"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+                <a href="index.php"><i class="fas fa-tachometer-alt"></i> <span>Dashboard</span></a>
             </li>
             
-            <li class="nav-header">Content Management</li>
+            <li class="nav-header"><span>Content Management</span></li>
             
             <?php if ($current_user && in_array($current_user['role'], ['admin', 'editor'])): ?>
                 <li class="<?php echo in_array($current_page, ['articles.php', 'add-article.php', 'edit-article.php']) ? 'active' : ''; ?>">
-                    <a href="articles.php"><i class="fas fa-newspaper"></i> Articles</a>
+                    <a href="articles.php"><i class="fas fa-newspaper"></i> <span>Articles</span></a>
                 </li>
                 
                 <li class="<?php echo in_array($current_page, ['pages.php', 'add-page.php', 'edit-page.php']) ? 'active' : ''; ?>">
-                    <a href="pages.php"><i class="fas fa-file-alt"></i> Pages</a>
+                    <a href="pages.php"><i class="fas fa-file-alt"></i> <span>Pages</span></a>
                 </li>
                 
                 <li class="<?php echo in_array($current_page, ['categories.php', 'add-category.php', 'edit-category.php']) ? 'active' : ''; ?>">
-                    <a href="categories.php"><i class="fas fa-folder"></i> Categories</a>
+                    <a href="categories.php"><i class="fas fa-folder"></i> <span>Categories</span></a>
                 </li>
 
                 <li class="<?php echo in_array($current_page, ['media.php', 'add-media.php', 'edit-media.php']) ? 'active' : ''; ?>">
-                    <a href="media.php"><i class="fas fa-photo-video"></i> Media</a>
+                    <a href="media.php"><i class="fas fa-photo-video"></i> <span>Media</span></a>
                 </li>
             <?php endif; ?>
             
             <li class="<?php echo in_array($current_page, ['comments.php', 'edit-comment.php']) ? 'active' : ''; ?>">
-                <a href="comments.php"><i class="fas fa-comments"></i> Comments</a>
+                <a href="comments.php"><i class="fas fa-comments"></i> <span>Comments</span></a>
             </li>
             
             <?php if ($current_user && $current_user['role'] === 'admin'): ?>
-                <li class="nav-header">Administration</li>
+                <li class="nav-header"><span>Administration</span></li>
                 
                 <li class="<?php echo in_array($current_page, ['users.php', 'add-user.php', 'edit-user.php']) ? 'active' : ''; ?>">
-                    <a href="users.php"><i class="fas fa-users"></i> Users</a>
+                    <a href="users.php"><i class="fas fa-users"></i> <span>Users</span></a>
                 </li>
                 
                 <li class="<?php echo $current_page === 'settings.php' ? 'active' : ''; ?>">
-                    <a href="settings.php"><i class="fas fa-cog"></i> Settings</a>
+                    <a href="settings.php"><i class="fas fa-cog"></i> <span>Settings</span></a>
                 </li>
             <?php endif; ?>
         </ul>
